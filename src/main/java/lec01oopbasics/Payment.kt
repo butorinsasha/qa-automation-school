a6688a6e8fbf7e6dc481d674f75f66bad63716ba
butorinsasha <40592660+butorinsasha@users.noreply.github.com>
2019-02-01 18:22:02 +0300

    Update Payment.kt

--- src/main/java/lec01oopbasics/Payment.kt
+++ src/main/java/lec01oopbasics/Payment.kt
@@ -3,7 +3,7 @@ package lec01oopbasics
 class Payment {
     val id: String = "00001"
     val sum: Float = 123.45f
-    val user: String = "Sasha"
+    val user: String = "Aleksandr"
     val shopId: String = "1337"
 
     fun preparePayment() {
