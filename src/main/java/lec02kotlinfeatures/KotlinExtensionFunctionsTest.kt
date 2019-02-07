5d2a2c171ef7f584758ba6eabd9fdcd68b4bbb46
Aleksandr Butorin <butorinsasha@gmail.com>
2019-02-08 02:29:05 +0300

    KotlinExceptions

--- src/main/java/lec02kotlinfeatures/KotlinExtensionFunctionsTest.kt
+++ src/main/java/lec02kotlinfeatures/KotlinExtensionFunctionsTest.kt
@@ -5,7 +5,7 @@ import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
-class ExtensionFunctionsTest {
+class KotlinExtensionFunctionsTest {
 
     // Traditional solution
 
