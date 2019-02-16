cd89e47f92dbeef381d704558a765387a5feef97
butorinab <butorinab@yamoney.ru>
2019-02-16 19:59:09 +0300

    'balance' with 'getUserBalance()' by parsing 'operations' text file and calculating balance by operation type

--- src/main/java/lec04app/Operations.kt
+++ src/main/java/lec04app/Operations.kt
@@ -6,15 +6,26 @@ import java.math.BigDecimal
 interface Operation {
     val sum: BigDecimal
     val user: String
+    fun newBalance(oldBalance: BigDecimal): BigDecimal
 }
 
 data class Deposit(
         override val user: String,
         override val sum: BigDecimal
-) : Operation
+) : Operation {
+    override fun newBalance(oldBalance: BigDecimal): BigDecimal {
+        // return oldBalance.plus(sum)
+        // same as following...
+        return oldBalance + sum
+    }
+}
 
 data class Payment(
         override val user: String,
         override val sum: BigDecimal,
         val shopId: String
-) : Operation
+) : Operation {
+    override fun newBalance(oldBalance: BigDecimal): BigDecimal {
+        return oldBalance - sum
+    }
+}
